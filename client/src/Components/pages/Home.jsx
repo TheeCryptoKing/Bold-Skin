0e91c35acf856f507805476a9feaf0505d8517bd
kwamebrowne <theeecryptoking@gmail.com>
2023-07-11 22:25:18 -0500

    Login and Signup form created and now Fixing Images

---
+++ client/src/Components/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React, { useContext, useEffect, useState } from "react";
+const Home = () => {
+    return (
+      <div>
+        <h1>Welcome to the Current Landing Page</h1>
+      </div>
+    );
+  };
+  
+  export default Home;
